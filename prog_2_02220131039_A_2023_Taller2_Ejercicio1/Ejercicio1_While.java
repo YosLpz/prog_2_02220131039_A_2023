021d47bd1f2df22a7e9b489546ef34c87ae048d9
YosLpz <124361225+YosLpz@users.noreply.github.com>
2023-02-17 10:00:27 -0500

    Add files via upload

---
+++ prog_2_02220131039_A_2023_Taller2_Ejercicio1/Ejercicio1_While.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+public class Ejercicio1_While {
+    public static void main(String[] args) {
+        
+        int num1 = 1;
+
+        Random r = new Random();
+    
+        num1 = (int)(r.nextDouble() * 2023);
+    
+        System.out.println("El multiplicando es " + num1);
+
+        int num2 = 1;
+        
+        num2 = (int)(r.nextDouble() * 2023);
+
+        System.out.println("El multiplicador es " + num2);
+
+        int operacion = 0;
+        
+        int num3 = 0;
+
+        while (num3 <= num2){
+            
+            operacion = num1 * num3;
+
+            System.out.println("\n" + num1 + " x " + num3 + " = " + operacion);
+
+            num3++;
+        }
+    }
+}
